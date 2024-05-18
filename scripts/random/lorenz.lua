0c42d4e1130b86e171e8d03ebe89028d3a1420f1
Jonah <105020079+jonahwhat@users.noreply.github.com>
2024-05-18 01:46:01 -0400

    Update lorenz attractor script readability

--- scripts/random/lorenz.lua
+++ scripts/random/lorenz.lua
@@ -1,41 +1,42 @@
+-- plots a lorenz attractor
 -- https://en.wikipedia.org/wiki/Lorenz_system
 
 $once$
 
 -- variables
 local size = $float(Size, 2.5, 0.5, 5)$
-local sigma = $int(sigma, 10, 0, 100)$
-local rho = $int(rho, 28, 0, 100)$
-local beta = $float(beta, 2.666666666, -1.0, 10.0)$
+local sigma = $int(Sigma, 10, 0, 100)$
+local rho = $int(Rho, 28, 0, 100)$
+local beta = $float(Beta, 2.666666666, -1.0, 10.0)$
 local block = $blockState(Block, stone)$
 
 -- constants
-local step_size = 0.01
-local num_points = 20000
+local num_points = 10000
 local y_offset = 50
+local d_t = 0.01
 
 -- starting position
 local x_t = 0.1
 local y_t = 0
 local z_t = 0
 
--- buncha math to determine the next position given an (x,y,z) position
-local function lorenz(x_t, y_t, z_t)
+-- function to compute the next position using the Lorenz system equations
+local function lorenz(x_t, y_t, z_t, sigma, rho, beta, d_t)
     local x1 = sigma * (y_t - x_t)
     local y1 = x_t * (rho - z_t) - y_t
     local z1 = (x_t * y_t - beta * z_t)
     
-    return x_t + step_size * x1, y_t + step_size * y1, z_t + step_size * z1
+    return x_t + (d_t * x1), y_t + (d_t * y1), z_t + (d_t * z1)
 end
 
-
+-- main loop
 for i=1, num_points do
- 	x_t, y_t, z_t = lorenz(x_t, y_t, z_t)
+    x_t, y_t, z_t = lorenz(x_t, y_t, z_t, sigma, rho, beta, d_t)
 
-    local xo = math.floor(x_t * size)
-    local yo = math.floor(z_t * size)
-    local zo = math.floor(y_t * size)
+    local x_final = math.floor(x_t * size) + x
+    local y_final = math.floor(z_t * size) + y + y_offset
+    local z_final = math.floor(y_t * size) + z
     
-    setBlock(xo + x, y_offset + yo + y, zo + z, block)
+    setBlock(x_final, y_final, z_final, block)
 end
 
